bce8eb599d38af12092e54e457650102e5b6a929
dooyou21 <cubeclock94@gmail.com>
2019-09-19 03:36:00 +0900

    ENH: api연동하여 실제 데이터를 받아오도록 구현(+스타일링)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatsComponent } from './cats/cats.component';
 import { ViewerComponent } from './viewer/viewer.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CatsComponent,
-    ViewerComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CatsComponent, ViewerComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/cat.service.ts
+++ src/app/cat.service.ts
@@ -1,23 +1,26 @@
 import { Injectable } from '@angular/core';
 import Cat from './class/cat';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CatService {
-  constructor() {}
+  private baseUrl = 'https://api.thecatapi.com/v1/';
+  headers = new HttpHeaders({ 'x-api-key': '5f845da8-8e75-4d87-9d7c-e890be64a2c2' });
+
+  constructor(private http: HttpClient) {}
 
   getCats(): Observable<Cat[]> {
-    return of([
-      {
-        id: 'MTgyMTg3Mw',
-        url: 'https://cdn2.thecatapi.com/images/MTgyMTg3Mw.jpg',
-      },
-      {
-        id: 'MTgyMTg3Mw',
-        url: 'https://cdn2.thecatapi.com/images/MTgyMTg3Mw.jpg',
-      },
-    ]);
+    const path = 'images/search';
+    const params = new HttpParams().set('limit', '30').set('size', 'small');
+    return this.http.get<Cat[]>(`${this.baseUrl}${path}`, { headers: this.headers, params });
+  }
+
+  getCat(id: string): Observable<Cat> {
+    const path = 'images/search';
+    const params = new HttpParams().set('image_id', id);
+    return this.http.get<Cat>(`${this.baseUrl}${path}`, { headers: this.headers, params });
   }
 }
