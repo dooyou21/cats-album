5b01a251ec0040b031b891e16f7053d774c833ea
dooyou21 <cubeclock94@gmail.com>
2019-10-03 18:54:19 +0900

    ENH: infinity scroll 구현

--- src/app/cats/cats.component.ts
+++ src/app/cats/cats.component.ts
@@ -17,6 +17,16 @@ export class CatsComponent implements OnInit {
   }
 
   getCats(): void {
-    this.catService.getCats().subscribe(cats => (this.cats = cats));
+    this.catService.getCats().subscribe(cats => (this.cats = this.cats.concat(cats)));
+  }
+
+  onScroll(e) {
+    // TODO: HostListener 찾아보기. body scroll 받아오는 방법은 없는지
+    const { scrollHeight, scrollTop, clientHeight } = e.target;
+    const scrollPercent = (scrollTop / (scrollHeight - clientHeight)) * 100;
+    if (scrollPercent === 100) {
+      // TODO: throttle 어떻게 걸지... 70%일때 한번 받아오는 걸 하고싶은데
+      this.getCats();
+    }
   }
 }
